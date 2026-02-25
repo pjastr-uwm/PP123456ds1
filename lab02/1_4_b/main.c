325b1ebc113a2266f601e1ac483988bc3d02d1d5
pjastr <114875756+pjastr-uwm@users.noreply.github.com>
2026-02-25 14:18:11 +0100

    1_4

---
+++ lab02/1_4_b/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b,c;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    scanf("%d", &c);
+    printf("Srednia %d %d %d to %f", a,b,c,(a+b+c)/3.0);
+    return 0;
+}
