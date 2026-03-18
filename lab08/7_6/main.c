80b31178e478c43d800ea2e742c65c10f7fd4867
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2026-03-18 14:29:28 +0100

    7_6

---
+++ lab08/7_6/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+float calculatePowerOfTwo(int n){
+    float temp=1;
+    if (n>=0){
+        for(int i=1;i<=n;i++){
+            temp *=2;
+        }
+        return temp;
+    }
+    for(int i=-1;i>=n;i--){
+        temp /=2;
+    }
+    return temp;
+}
+
+int main()
+{
+    printf("%f\n", calculatePowerOfTwo(4));
+    printf("%f\n", calculatePowerOfTwo(0));
+    printf("%f\n", calculatePowerOfTwo(-2));
+    return 0;
+}
