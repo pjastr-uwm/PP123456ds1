3090fbc250eaa5aa2c44e8e2501f435bb6da14e2
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2026-03-30 12:28:02 +0200

    8_13

---
+++ lab11/8_13/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int* minPtr(int *p1, int* p2, int* p3){
+    int min = *p1;
+    int* ptr = p1;
+    if (min > * p2){
+        min = *p2;
+        ptr = p2;
+    }
+    if (min > * p3){
+        min = *p3;
+        ptr = p3;
+    }
+    return ptr;
+}
+
+int main()
+{
+    int x = 3, y=-20, z= -20;
+    printf("%d %p\n", x, &x);
+    printf("%d %p\n", y, &y);
+    printf("%d %p\n", z, &z);
+    printf("%p\n", minPtr(&x, &y, &z));
+    return 0;
+}
