937de87497962b3c3c06e55f7c07d8228e238bcf
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2026-04-13 12:53:09 +0200

    10_06

---
+++ lab13/10_06/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void reverseArr(int n, int tab[]){
+    for(int i=0;i<n/2;i++){
+        int temp = tab[i];
+        tab[i] = tab[n-1-i];
+        tab[n-1-i] = temp;
+    }
+}
+
+void printTable(int n,int  tab[]){
+    for(int i=0;i<n;i++){
+        printf("%d ", tab[i]);
+    }
+    printf("\n------------\n");
+}
+
+
+int main()
+{
+    int tab[] = {3,-6,4,8,7};
+    printTable(5,tab);
+    reverseArr(5, tab);
+    printTable(5,tab);
+    return 0;
+}
