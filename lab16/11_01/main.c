c143e4a5183cc64817f8fad4429194c0a02cce32
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2026-04-22 14:08:09 +0200

    110_1

--- lab16/11_01/main.c
+++ lab16/11_01/main.c
@@ -36,11 +36,19 @@ int length4(char * txt){
     return i;
 }
 
+int length5(char * txt){
+    if (txt[0] == 0){
+        return 0;
+    }
+    return 1+length5(++txt);
+}
+
 int main()
 {
     printf("%d\n", length("ABC"));
     printf("%d\n", length2("ABC"));
     printf("%d\n", length3("ABC"));
     printf("%d\n", length4("ABC"));
+    printf("%d\n", length5("ABC"));
     return 0;
 }
