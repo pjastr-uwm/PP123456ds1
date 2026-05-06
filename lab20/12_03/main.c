93e4b77b7dbf45bb7a2f8b51d25d3aca155d208a
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2026-05-06 14:19:20 +0200

    12_03

--- lab20/12_03/main.c
+++ lab20/12_03/main.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int maxRowIdx(int n, int m, int tab[n][m]){
+    int index =0;
+    int max = tab[0][0];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if (max < tab[i][j]){
+                max = tab[i][j];
+                index = i;
+            }
+        }
+    }
+    return index;
+}
 
 int main()
 {
-
+    int tab[3][3] = {{3,4,2},{-4,2,1},{0,-4,4}};
+    printf("%d\n", maxRowIdx(3,3,tab));
     return 0;
 }
